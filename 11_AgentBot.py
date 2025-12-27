e0f7d5173f7dd4803adda78bddafbcb9e42be763
aghori3004 <divyanshgangwar3004@gmail.com>
2025-12-27 16:14:24 +0530

    added lec 9,10,11 and a gitignore file

---
+++ 11_AgentBot.py
@@ -0,0 +1,5 @@
+from typing import TypedDict, List
+from langchain_core.messages import HumanMessag
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgrapg.graph import StateGraph, START, END
+from dotenv import load_dotenv
